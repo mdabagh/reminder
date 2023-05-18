6b996d221b475a5cb039f24d7235c3de49a49cab
mdabagh <mohammadsajaddabbagh@gmail.com>
2023-05-18 09:11:49 +0330

    change db user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone' , 13)->unique();
+            $table->string('phone' , 13)->unique()->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
